8fee0949258d5e24ca685f367d78c08d7c39097f
gze1206 <gze1206@naver.com>
2016-10-03 23:07:49 +0900

    3
    fizzBuzz 되지도 않는거 그냥 빼버렸습니다.
    HTML로 페이지 갈아치움

--- app.js
+++ app.js
@@ -2,30 +2,7 @@ var express = require('express');
 
 var app = express();
 
-function fizzBuzz()
-{
-  for (i=1; i<=100; i++)
-  {
-    if (i%3 === 0 && i%5 === 0)
-    {
-      res.sned('fizzBuzz');
-    }
-    else if (i%3 === 0)
-    {
-      res.send('Fizz');
-    }
-    else if (i%5 === 0)
-    {
-      res.send('Buzz');
-    }
-    else res.send(i);
-  }
-}
-
-app.get('/', function (req,res) {
-  res.send('Hello World!');
-  fizzBuzz();
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function () {
   console.log('Server On!');
