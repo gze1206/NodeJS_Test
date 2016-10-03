a336e3804a47f9520b1bb5640daf4d9f8a05f192
gze1206 <gze1206@naver.com>
2016-10-03 22:56:09 +0900

    3
    서버 접속시(localhost::3000) fizzBuzz 실행

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ var express = require('express');
 
 var app = express();
 
+function fizzBuzz()
+{
+  for (i=1; i<=100; i++)
+  {
+    if (i%3 === 0 && i%5 === 0)
+    {
+      res.sned('fizzBuzz');
+    }
+    else if (i%3 === 0)
+    {
+      res.send('Fizz');
+    }
+    else if (i%5 === 0)
+    {
+      res.send('Buzz');
+    }
+    else res.send(i);
+  }
+}
+
 app.get('/', function (req,res) {
   res.send('Hello World!');
+  fizzBuzz();
 });
 
 app.listen(3000, function () {
